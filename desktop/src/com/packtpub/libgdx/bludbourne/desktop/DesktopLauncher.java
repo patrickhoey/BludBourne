4ee1dfa9625d1a7ba92b68361e0bb70cda9189e2
phoey <patrick.hoey@gmail.com>
2015-04-04 13:40:03 -0700

    Initial Commit

---
+++ desktop/src/com/packtpub/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packtpub.libgdx.bludbourne.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.libgdx.bludbourne.BludBourne;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BludBourne(), config);
+	}
+}
