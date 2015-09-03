e9628eaf4c413aae8a6bf3ca448753a49671069b
phoey <patrick.hoey@gmail.com>
2015-09-03 13:54:32 -0700

    Ch09
    1) Few minor changes for values; Added log statements; Seeded random angle upon initial instantiation

--- core/src/com/packtpub/libgdx/bludbourne/sfx/ShakeCamera.java
+++ core/src/com/packtpub/libgdx/bludbourne/sfx/ShakeCamera.java
@@ -1,5 +1,6 @@
 package com.packtpub.libgdx.bludbourne.sfx;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
@@ -21,6 +22,7 @@ public class ShakeCamera {
 		this._origShakeRadius = shakeRadius;
 		this._offset = new Vector2();
 		this._currentPosition = new Vector2();
+		reset();
 	}
 	
 	public boolean isCameraShaking(){
@@ -36,26 +38,41 @@ public class ShakeCamera {
 	}
 	
 	private void computeCameraOffset(){
-		_offset.x =  MathUtils.sinDeg(_randomAngle) * _shakeRadius;
-		_offset.y =  MathUtils.cosDeg(_randomAngle) * _shakeRadius;
+		float sine = MathUtils.sinDeg(_randomAngle);
+		float cosine = MathUtils.cosDeg(_randomAngle);
+
+		//Gdx.app.log(TAG, "Sine of " + _randomAngle + " is: " + sine);
+		//Gdx.app.log(TAG, "Cosine of " + _randomAngle + " is: " + cosine);
+
+		_offset.x =  cosine * _shakeRadius;
+		_offset.y =  sine * _shakeRadius;
+
+		//Gdx.app.log(TAG, "Offset is x:" + _offset.x + " , y: " + _offset.y );
 	}
 	
 	private void computeCurrentPosition(){
 		_currentPosition.x = _origPosition.x + _offset.x;
 		_currentPosition.y = _origPosition.y + _offset.y;
+
+		//Gdx.app.log(TAG, "Current position is x:" + _currentPosition.x + " , y: " + _currentPosition.y );
 	}
 	
 	private void diminishShake(){
+		//Gdx.app.log(TAG, "Current shakeRadius is: " + _shakeRadius + " randomAngle is: " + _randomAngle);
+
 		if( _shakeRadius < 2.0 ){
+			//Gdx.app.log(TAG, "Done shaking");
 			reset();
 			return;
 		}
 		
-		//Gdx.app.log(TAG, "Current shakeRadius is: " + _shakeRadius + " randomAngle is: " + _randomAngle);
-		
 		_isShaking = true;
 		_shakeRadius *= .9f;
+		//Gdx.app.log(TAG, "New shakeRadius is: " + _shakeRadius);
+
+
 		_randomAngle = MathUtils.random(1, 360);
+		//Gdx.app.log(TAG, "New random angle: " + _randomAngle);
 	}
 
 	public void reset(){
